3e1d497b6808169bb5aa06394fb5ee18019e4c6d
Niel McLaren <dan@danielmclaren.com>
2015-08-03 20:55:58 -0700

    Better reporting.

--- bieberbot.go
+++ bieberbot.go
@@ -17,7 +17,7 @@ func main() {
 	bieberLovePattern = regexp.MustCompile(`i love[^.!?]*(justin)?bieber`)
 
 	http.HandleFunc("/hook", hook)
-	fmt.Println("listening...")
+	log.Println("Looking for Bieber love...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		panic(err)
@@ -25,24 +25,28 @@ func main() {
 }
 
 func hook(res http.ResponseWriter, req *http.Request) {
-	fmt.Println("hooked")
 	if req.Method == "POST" {
-		buffer := new(bytes.Buffer)
-		buffer.ReadFrom(req.Body)
-		msg, err := url.ParseQuery(buffer.String())
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println("user ID: " + msg["user_id"][0])
-		fmt.Println("user name: " + msg["user_name"][0])
-		if lovesJustinBieber(msg["text"][0]) {
-			log.Printf(
-				"#%s user:%s (%s), \"%s\" (\"%s\")\n",
-				msg["channel_name"][0], msg["user_id"][0], msg["user_name"][0], msg["text"][0], msg["trigger_word"][0])
-			fmt.Fprintf(res, "{\"text\": \"Oh, I love you, too, @%s.\"}", msg["user_name"][0])
+		msg := parseRequest(req)
+		if msg != nil {
+			lovesBieber := lovesJustinBieber(msg["text"][0])
+			log.Printf("Love found! user=%s, channel=%s, bieber=%t, text=\"%s\"", msg["user_name"][0], msg["channel_name"][0], lovesBieber, msg["text"][0])
+			if lovesBieber {
+				fmt.Fprintf(res, "{\"text\": \"I love you, too, @%s.\"}", msg["user_name"][0])
+			}
 		}
 	}
+}
+
+func parseRequest(req *http.Request) map[string][]string {
+	b := new(bytes.Buffer)
+	b.ReadFrom(req.Body)
+	s := b.String()
+	msg, err := url.ParseQuery(s)
+	if err != nil {
+		log.Printf("Bad webhook request. data=%s", s)
+		return nil
+	}
+	return msg
 }
 
 func lovesJustinBieber(text string) bool {
